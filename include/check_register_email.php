4a4d49e07f33bad8114ffc54455ffa871732c61c
ravsha21 <52937253+ravsha21@users.noreply.github.com>
2019-10-28 16:04:52 -0700

    Add files via upload
    file for email validation

---
+++ include/check_register_email.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+include_once "../autoload.php";
+
+if (isset($_POST['email'])){ 
+    $email = $_POST['email'];
+}
+
+$user = new User;
+$user->checkemail($email);
+//header("Location: ../login_register.php?");
