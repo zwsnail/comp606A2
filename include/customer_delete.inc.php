65c8e0ae41e5b01e95b49b6a24866829f8b54682
ravsha21 <52937253+ravsha21@users.noreply.github.com>
2019-11-02 02:23:32 +1300

    Update customer_delete.inc.php
    delete customer

--- include/customer_delete.inc.php
+++ include/customer_delete.inc.php
@@ -1,14 +1,10 @@
 <?php
 session_start();
-include_once "../autoload.php";
+include_once "../helper/autoloader.php";
 
 
 //getting id of the data from url
 $job_id = $_GET['job_id'];
-
-
-$job = new Job;
-$job->customer_delete_job($job_id);
-
+$job = Job::customer_delete_job($mysqli, $job_id);
 
 header("Location: ../customer_job_detail.php");
