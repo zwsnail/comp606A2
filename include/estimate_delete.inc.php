26ec91f77a2ab8542e93d2e1839527d4798a20ba
ravsha21 <52937253+ravsha21@users.noreply.github.com>
2019-11-02 02:24:48 +1300

    Update estimate_delete.inc.php
    delete bid

--- include/estimate_delete.inc.php
+++ include/estimate_delete.inc.php
@@ -1,16 +1,13 @@
 <?php
 session_start();
-include_once "../autoload.php";
+include_once "../helper/autoloader.php";
 
 
 //getting id of the data from url
 $estimate_id = $_GET['id'];
 $job_id = $_GET['job_id'];
-$job_status = $_GET['job_status'];
+//$job_status = $_GET['job_status'];
 
-
-$estimate = new Estimate;
-$estimate->trademan_delete_bid($estimate_id, $job_id);
-
+$estimate = Estimate::trademan_delete_bid($mysqli, $estimate_id, $job_id);
 
 header("Location: ../trademan_bid_joblist.php");
