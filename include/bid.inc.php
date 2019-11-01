138e71b2c62dd4969eb40966c39dcaf50cdc4025
ravsha21 <52937253+ravsha21@users.noreply.github.com>
2019-11-02 02:22:20 +1300

    Update bid.inc.php
    changes in bid file

--- include/bid.inc.php
+++ include/bid.inc.php
@@ -5,7 +5,7 @@
     Then use the class Job 'trademan_bid' function to change the job status from "No one bid" to "Got a bid"
 */
 session_start();
-require_once "../autoload.php";
+include_once "../helper/autoloader.php";
 
 $job_id= $_SESSION['job_id'];
 // var_dump($job_id);
@@ -18,7 +18,7 @@ $trademan_id = $_SESSION['uid'];
 // var_dump($trademan_id);
 
 $job_status = $_SESSION['job_status'];
-var_dump($job_status);
+//var_dump($job_status);
 
 $material_cost = $_POST['material_price'];
 $labor_cost = $_POST['labor_price'];
@@ -26,8 +26,8 @@ $total_cost = $_POST['toal_price'];
 $starting_date = $_POST['start_time'];
 $expiring_date = $_POST['expire_time'];
 
-$estimate = new Estimate;
-$estimate->create_estimate($job_id, $trademan_id, $material_cost, $labor_cost, $total_cost, $starting_date, $expiring_date) ;
+
+$estimate = Estimate::create_estimate($mysqli, $job_id, $trademan_id, $material_cost, $labor_cost, $total_cost, $starting_date, $expiring_date);
 // $job = new Job;
 // $job->trademan_bid($job_id, $trademan_id, $user_id, $job_status);
 
