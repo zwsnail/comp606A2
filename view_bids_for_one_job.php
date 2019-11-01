d95904ee7db8ffcf5c476eaf36554fb548d5f6f4
ravsha21 <52937253+ravsha21@users.noreply.github.com>
2019-11-02 02:34:06 +1300

    Update view_bids_for_one_job.php
    add some functionality to show bids

--- view_bids_for_one_job.php
+++ view_bids_for_one_job.php
@@ -3,7 +3,8 @@
     This is page for displaying all bids by only single job.
 */
 session_start();
-require_once "autoload.php";
+include_once "database/autoloader.php";
+include_once "database/connection.php";
 require_once "header.php";
 
 $job_id = $_GET['job_id'];
@@ -19,8 +20,8 @@ if($job_status == 'Got bid')
     echo '<div class="container text-center">';
     echo '<h2 class="font-italic text-success">Your job '.$job_description.' got bid! Below are the details: </h2><br>';
     echo '<h6>Tip: You can find the contact by clicking the trademan ID :)</h6>';
-    $bid = new Estimate;
-    $bid->customer_view_estimate($job_id);
+    $bid = Estimate::customer_view_estimate($mysqli, $job_id);
+    
 }else
 {
     echo '<br><br>';
