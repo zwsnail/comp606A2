5e67638349016988d26f6969b9c9010661b4e5a3
ravsha21 <52937253+ravsha21@users.noreply.github.com>
2019-11-02 02:26:06 +1300

    Update register.inc.php
    registration values

--- include/register.inc.php
+++ include/register.inc.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include_once "../autoload.php";
+include_once "../helper/autoloader.php";
 
 
 $username = $_POST['name'];
@@ -12,6 +12,28 @@ $type = $_POST['type'];
 $_SESSION['type'] = $type;
 $password =  md5($_POST['pass']);
 
-$user = new User;
-$user->register($username, $mobile, $email, $type, $password);
-header("Location: ../login_register.php?");
+/*$user = new User;
+$user->register($username, $mobile, $email, $type, $password); */
+
+$newUser = User::register($mysqli, $username,$mobile,$email,$type,$password);
+
+/*if (is_null($newUser)){
+    "<h2>failed to create new student</h2>";
+} else {
+    echo "<h2>New User Registered</h2>";
+   
+}*/
+
+
+if (is_null($newUser)) {
+    //"Registration Failed";
+    header("Location: ../login_register.php?fail");
+  }
+  elseif ($newUser=='0') {
+    //  "alredy prenst"
+    header("Location: ../login_register.php?already");
+  }
+  else {
+    // Registration Success
+    header("Location: ../login_register.php?");
+  }
