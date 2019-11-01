48b6c3d4e23bdc47c22d44980492b848063dcc55
ravsha21 <52937253+ravsha21@users.noreply.github.com>
2019-11-02 02:32:02 +1300

    Update trademan_bid_joblist.php
    set data

--- trademan_bid_joblist.php
+++ trademan_bid_joblist.php
@@ -3,7 +3,8 @@
     This page is for display trademan's information
 */
 session_start();
-include_once "autoload.php";
+include_once "database/autoloader.php";
+include_once "database/connection.php";
 
 include('header.php');
 
@@ -14,12 +15,7 @@ echo '<div class="container text-center">';
 // echo '<h1 class="font-weight-bold text-primary">The jobs you have bid </h1>';
 echo '<h2 class="text-primary">The jobs you have bid</h2>';
 
-
-
-$estimate = new Estimate;
-$estimate->trademan_view_estimate($uid);
-
-
+$estimate = Estimate::trademan_view_estimate($mysqli, $uid);
 echo '</div>';
 echo '<br>';
 echo '<br>';
@@ -30,9 +26,7 @@ echo '<p> </p>';
 echo '<h2 class="text-primary">Total jobs in SafeTrade</h2>';
 echo '<h4 class="font-italic text-success">Please click the <b>Bid Bottom</b> if you are intested in the job!</h4>';
 
-$job = new Job;
-$job->trademan_view_job($uid);
-
+$job = Job::trademan_view_job($mysqli, $uid);
 
 // include('footer.php');
 
