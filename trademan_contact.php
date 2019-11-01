3f264e00f6a8514c68469f1deda86b94500b05ea
ravsha21 <52937253+ravsha21@users.noreply.github.com>
2019-11-02 02:32:56 +1300

    Update trademan_contact.php
    change links

--- trademan_contact.php
+++ trademan_contact.php
@@ -4,7 +4,8 @@
     And customer can click the button to comfirm this estimate of bid.
 */
 session_start();
-require_once "autoload.php";
+include_once "database/autoloader.php";
+include_once "database/connection.php";
 require_once "header.php";
 
 $trademan_id = $_GET['trademan_id'];
@@ -16,8 +17,8 @@ if($trademan_id != 0)
 {
     echo '<div class="container text-center">';
     echo '<h4 class="text-dark">Good luck!</h6>';
-    $trademan = new User;
-    $trademan ->view_trademan_contact($trademan_id);
+    
+    $trademan = User::view_trademan_contact($mysqli, $trademan_id);
 
     //a button for confirming with this bid
     echo '<a href="confirm.php?trademan_id='.$trademan_id.'"><button class="btn btn-outline-primary">Confirm it!</button></a>';
