1c9296584c6e11398de2f5020a56767fc717d2cb
ravsha21 <52937253+ravsha21@users.noreply.github.com>
2019-11-02 02:25:26 +1300

    Update login.inc.php
    login user

--- include/login.inc.php
+++ include/login.inc.php
@@ -1,27 +1,21 @@
 <?php
-include_once "../autoload.php";
-
-
-
-
+include_once "../helper/autoloader.php";
 $username = $_POST['username'];
 $password = md5($_POST['password']);  
 // var_dump($username);
-
-
-$user = new User;
-$user->login($username, $password);
-
-
+/*$user = new User;
+$user->login($username, $password);*/
 $type = $_SESSION['type'];
 $uid = $_SESSION['uid'];
 $name = $_SESSION['name'];
-if(isset($name))
-{
-    header("Location: ../welcome.php?");
-}else{
-    header("Location: ../login_register.php?error");
-}
+$newUser = User::login($mysqli, $username,$password);
+//print_r($newUser);
+//var_dump($newUser);
+//die("m here");
 
-// header("Location: ../welcome.php?<?php echo '$username';?>");
-<!-- header("Location: ../index.php?error=sqlerror"); -->
+if(is_null($newUser)) {
+    header("Location: ../login_register.php?error");
+} else {
+    echo "<h2>Login successfully</h2>";
+    header("Location: ../welcome.php?");
+}
